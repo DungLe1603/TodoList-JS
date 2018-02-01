81f9808c2933f084777ad624e9d18176b300b6f6
Dung Le <dungle160396@gmail.com>
2018-02-01 07:56:06 +0700

    nothing

---
+++ js/login.js
@@ -0,0 +1,23 @@
+
+function cancel(){
+  window.location.assign("login.html");
+}
+
+ function login() {
+ //    var xhttp = new XMLHttpRequest(); 
+ //    var url = "https://todo-js-be.herokuapp.com/auth/sign_in";
+ //    var email = document.getElementById("email").value;
+ //    var password = document.getElementById("password").value;
+ //    console.log("email = "+email+", pass = "+password);
+ //    var url = url + "?email=" + email + "&password=" + password;  
+
+ //    xhttp.open("POST", url, true);
+ //    // xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+ //    xhttp.send();
+ //  	 xhttp.onreadystatechange = function() {
+ //  	    if (xhttp.readyState == XMLHttpRequest.DONE) {
+ //  	        window.location.assign("todo.html");
+ //  	    }
+ //    }
+  window.location.assign("todo.html");
+}
